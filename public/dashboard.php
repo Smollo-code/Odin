3483c3caa32c1aa72b7a7525c44a444841eedc2f
FlorianFaust <florian.faust@simovative.com>
2023-11-28 13:27:42 +0100

    Funktioniert alles

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,5 +1,7 @@
 <?php
 require '../vendor/autoload.php';
+
+
 $loader = new \Twig\Loader\FilesystemLoader('../src/User/Templates');
 $twig = new \Twig\Environment($loader, [
     'cache' => False,
--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,16 +1,14 @@
 document.getElementById("changeProfileForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    // Hier kann der Code für die Aktualisierung der Benutzerinformationen geschrieben werden
+    // Maxi mach mal Code
 
 });
 
 function confirmDelete() {
     var confirmation = document.getElementById("confirmDelete").value;
     if (confirmation === "LÖSCHEN") {
-        // Hier kann der Code für das Löschen des Kontos geschrieben werden
 
-        alert("Konto erfolgreich gelöscht!");
-    } else {
-        alert("Falsche Bestätigung. Konto wurde nicht gelöscht.");
+
+        window.location.replace("http://odin.scam/index.php");
     }
 }
