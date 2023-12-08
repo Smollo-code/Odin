9361069ff3b6bb0bd0bbdc3dc0597b95812b525d
Maximilian Neidel <maximilian.neidel@simovative.com>
2023-12-08 09:13:52 +0100

    drehung

--- public/scripts/roulette.js
+++ public/scripts/roulette.js
@@ -1,3 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var table = document.getElementById('roulette-table');
+
+    table.addEventListener('click', function(event) {
+        var clickedCell = event.target;
+
+        if (clickedCell.attributes. === '11') {
+            console.log('Geklickte Zelle:', clickedCell.textContent, clickedCell.className);
+        }
+    });
+});
+
+
+
 function goBack() {
     window.location.replace("http://odin.scam/dashboard");
 }
+
+function rotateDegreeNumber() {
+    return Math.random() * (9000 - 1000) + 1000
+}
+
